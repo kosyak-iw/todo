e939809034470e0c03eb39ad1d5e3b23f99ddee9
Konstantin Valynets <7963985@bk.ru>
2022-04-01 22:23:50 +0300

    init

---
+++ script.js
@@ -0,0 +1,52 @@
+// const todoListItem = document.createElement('div');
+// const itemComplete = document.createElement('div');
+// const itemName = document.createElement('div');
+// const itemClose = document.createElement('div');
+//
+// todoListItem.classList.add('todo-list__item');
+// itemComplete.classList.add('todo-list__item-complete');
+// itemName.classList.add('todo-list__item-name');
+// itemClose.classList.add('todo-list__item-close');
+//
+// itemClose.textContent = '×';
+//
+// todoListItem.appendChild(itemComplete);
+// todoListItem.appendChild(itemName);
+// todoListItem.appendChild(itemClose);
+//
+// document.querySelector('.todo-list')
+//     .appendChild(todoListItem);
+
+const inputName = document.querySelector('.creator__name-input');
+
+inputName.onkeydown = function (keyDownEvent) {
+    if (keyDownEvent.key === 'Enter') {
+        const todoName = inputName.value;
+        const newTodo = createTodoHtml(todoName);
+        const todoList = document.querySelector('.todo-list');
+        todoList.appendChild(newTodo)
+    }
+}
+
+function createHtml(htmlString) {
+    const element = document.createElement('div');
+
+    element.innerHTML = htmlString.trim();
+
+    return element.firstElementChild;
+}
+
+function completeTodo(todoElement) {
+    todoElement.classList.add('todo-list__item_completed');
+}
+
+function createTodoHtml(name) {
+    return createHtml(`
+<div class="todo-list__item">
+    <div class="todo-list__item-complete"></div>
+    <div class="todo-list__item-name ">${name}</div>
+    <div class="todo-list__item-close"> &#215;</div>
+</div>`);
+}
+
+
