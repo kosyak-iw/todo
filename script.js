da1f619ae4c6b9187abe9bb790787988bdcc0971
Konstantin Valynets <7963985@bk.ru>
2022-04-02 00:01:07 +0300

    init

--- script.js
+++ script.js
@@ -17,12 +17,34 @@
 // document.querySelector('.todo-list')
 //     .appendChild(todoListItem);
 
+const completeAllButton = document.querySelector('.creator__complete-all');
+completeAllButton.addEventListener('click', () => {
+    const allTodos = document.querySelectorAll('.todo-list__item');
+    for (const todo of allTodos) {
+        completeTodo(todo);
+    }
+})
+function completeTodo(todoElement) {
+    todoElement.classList.add('todo-list__item_completed');
+}
+
+
+
+
 const inputName = document.querySelector('.creator__name-input');
 
 inputName.onkeydown = function (keyDownEvent) {
     if (keyDownEvent.key === 'Enter') {
         const todoName = inputName.value;
         const newTodo = createTodoHtml(todoName);
+        const completeTodoButton = newTodo.querySelector('.todo-list__item-complete');
+        completeTodoButton.addEventListener('click', () => {
+            toggleTodoCompleteState(newTodo);
+        });
+        const removeTodoButton = newTodo.querySelector('.todo-list__item-close');
+        removeTodoButton.addEventListener('click', () => {
+            removeTodo(newTodo);
+        })
         const todoList = document.querySelector('.todo-list');
         todoList.appendChild(newTodo)
     }
@@ -36,17 +58,23 @@ function createHtml(htmlString) {
     return element.firstElementChild;
 }
 
-function completeTodo(todoElement) {
-    todoElement.classList.add('todo-list__item_completed');
+function toggleTodoCompleteState(todoElement) {
+    todoElement.classList.toggle('todo-list__item_completed');
+
+}
+
+function removeTodo(todoElement) {
+    todoElement.remove();
 }
 
 function createTodoHtml(name) {
     return createHtml(`
-<div class="todo-list__item">
-    <div class="todo-list__item-complete"></div>
-    <div class="todo-list__item-name ">${name}</div>
-    <div class="todo-list__item-close"> &#215;</div>
-</div>`);
+        <div class="todo-list__item">
+            <div class="todo-list__item-complete"></div>
+            <div class="todo-list__item-name">${name}</div>
+            <div class="todo-list__item-close"> &#215;</div>
+        </div>
+    `);
 }
 
 
